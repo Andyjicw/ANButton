f4285f162ec108e10e75031b0c716b9d4ddf7525
AndyJ <479003573@qq.com>
2016-11-21 18:41:48 +0800

    test

--- ANButtonDemo/ANButtonDemo/ViewController/ViewController.m
+++ ANButtonDemo/ANButtonDemo/ViewController/ViewController.m
@@ -20,7 +20,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    // test github
     
     [self example1];
     [self example2];
